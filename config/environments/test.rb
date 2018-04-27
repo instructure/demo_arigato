d6920e02c84738dff22170da790033530b3b194d
David Thalman <dhthalman@gmail.com>
2018-04-27 14:08:19 -0600

    Step 4.4 rails update ordering of test cases

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,8 +34,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.active_support.test_order = :sorted # or `:random` if you prefer
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { :host => "localhost" }
+
 end
