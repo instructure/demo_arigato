0a1f351f3e087520b9c36009eea16398bd4482e1
David Thalman <dhthalman@gmail.com>
2018-05-31 11:06:10 -0600

    Added attr_encrypted to Auth model

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,7 +1,13 @@
 class Authentication < ActiveRecord::Base
 
+require 'base64'
+
   belongs_to :user, :inverse_of => :authentications
 
   validates :provider, :presence => true, :uniqueness => {:scope => [:uid, :user_id, :provider_url]}
+
+  attr_encrypted :token, key: Base64.decode64(Rails.application.secrets.encrypt_token_key)
+
+  attr_encrypted :refresh_token, key: Base64.decode64(Rails.application.secrets.encrypt_refresh_token_key)
 
 end
